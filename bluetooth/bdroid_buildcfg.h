f35837134fc62022a498c9c1096f2df61a08ecc6
shivom007 <yashpaljoshi76@gmail.com>
2017-10-16 07:04:55 +0000

    Fix Bluetooth name

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,6 +1,6 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Panasonic P55 Novo-Shivom Techdroid"
+#define BTM_DEF_LOCAL_NAME   "P55-Novo-YJ_Shivom"
 
 #endif
